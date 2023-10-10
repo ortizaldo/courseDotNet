3f105d0c179fdd2c4a553bc7712af7b335d45e90
Aldo Ortiz <ortizaldo14@gmail.com>
2023-10-10 07:22:05 -0600

    first commit

---
+++ DatingApp/API/Entities/AppUser.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Entities;
+
+public class AppUser
+{
+  [Key]
+  public int Id { get; set; }
+  public string UserName { get; set; }
+  public string Firstname { get; set; }
+  public string Lastname { get; set; }
+  public string PhoneNumber { get; set; }
+}
